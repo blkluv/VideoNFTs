930adf01a9ec4f7128216b89164f2acffadcc76c
WEB5D <info@blkluv.org>
2026-06-04 02:46:27 -0400

    Update hardhat.config.js

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -12,9 +12,10 @@ export default {
   networks: {
     hardhat: {
     },
-    amoy: {
-      url: process.env.AMOY_RPC_URL || "https://rpc-amoy.polygon.technology/",
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+    base: {
+      url: process.env.BASE_RPC_URL || "https://mainnet.base.org",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 8453
     },
     localhost: {
       url: "http://127.0.0.1:8545",
